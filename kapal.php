c7492665fe0dddc900f854ed1b48cd482908c139
sitinurabidah <sitinurabidah24@gmail.com>
2019-04-26 11:34:32 +0700

    Penambahan kapal.php

---
+++ kapal.php
@@ -0,0 +1,152 @@
+<?php
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "nelayanondata";
+$link = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+    if(!$link){
+        die ("koneksi dengan database gagal: ".mysqli_connect_errno()." - ".mysqli_connect_error());
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Title -->
+    <title>Nelayan On Data</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="./img/core-img/favicon.png">
+
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+    <!-- Header Area Start -->
+    <header class="header-area">
+        <div class="classy-nav-container breakpoint-off">
+            <div class="container">
+                <!-- Classy Menu -->
+                <nav class="classy-navbar justify-content-between" id="conferNav">
+
+                    <!-- Logo -->
+                    <a class="nav-brand" href="./index.html"><img src="./img/core-img/logo.png" alt=""></a>
+
+                    <!-- Navbar Toggler -->
+                    <div class="classy-navbar-toggler">
+                        <span class="navbarToggler"><span></span><span></span><span></span></span>
+                    </div>
+
+                    <!-- Menu -->
+                    <div class="classy-menu">
+                        <!-- Menu Close Button -->
+                        <div class="classycloseIcon">
+                            <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                        </div>
+                        <!-- Nav Start -->
+                        <div class="classynav">
+                            <ul id="nav">
+                                <li ><a href="index.html">Home</a></li>
+                                <li><a href="awak.html">Awak Kapal</a></li>
+                                <li class="active"><a href="#">Kapal</a></li>
+                                <li><a href="#">Awak Hilang</a></li>
+                                <li><a href="#">About Us</a></li>
+                            </ul>
+
+                            <!-- Get Tickets Button -->
+                            <a href="#" class="btn confer-btn mt-3 mt-lg-0 ml-3 ml-lg-5">Login</i></a>
+                        </div>
+                        <!-- Nav End -->
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </header>
+    <!-- Header Area End -->
+
+<!-- Breadcrumb Area Start -->
+    <section class="breadcrumb-area bg-img bg-gradient-overlay jarallax" style="background-image: url(img/bg-img/2.jpg);">
+        <div class="container h-100">
+            <div class="row h-100 align-items-center">
+                <div class="col-12">
+                    <div class="breadcrumb-content">
+                        <h2 class="page-title">Kapal</h2>
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">Kapal</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Breadcrumb Area End -->
+       
+        <!-- tampilan  tabel -->
+        <div class="container">
+        
+        <table class="data-table col-12">
+        <caption class="title">Data kapal</caption>
+        <thead>
+            <tr>
+                <th>NO</th>
+                <th>Nama Kapal</th>
+                <th>Besar Muatan</th>
+                <th>Nahkoda</th>
+                <th>Kondisi</th>
+            </tr>
+        </thead>
+        <tbody>
+       <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+        </tbody>
+        <tfoot>   
+        </tfoot>
+    </table>
+
+    </div>
+    </section>
+
+    <!-- Footer Area Start -->
+    <footer class="footer-area bg-img bg-overlay-2">
+
+        <!-- Copywrite Area -->
+        <div class="container">
+            <div class="copywrite-content">
+                <div class="row">
+                    <!-- Copywrite Text -->
+                    <div class="col-12 col-md-12">
+                        <div class="copywrite-text">
+                            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- Footer Area End -->
+
+</body>
+
+</html>
